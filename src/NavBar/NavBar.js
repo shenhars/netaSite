235b894a5bd8935c8cc144f3d6bdb6ee29c592cf
shenhars <shenhars03@gmail.com>
2022-09-23 12:28:08 +0300

    phone nav bar changes and fontawesome delete

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -4,36 +4,43 @@ import React, { useState } from 'react';
 
 const NavBar = ({ srcs }) => {
     const phoneIcons = [{
-        icon: "user",
-        href: "עלי"
+        icon: 'user',
+        href: 'עלי'
     }, {
-        icon: "microphone",
-        href: "הופעות"
+        icon: 'microphone',
+        href: 'הופעות'
     }, {
-        icon: "play",
-        href: "מוזיקה"
+        icon: 'play',
+        href: 'מוזיקה'
     }, {
-        icon: "share",
-        href: "עוד"
+        icon: 'share',
+        href: 'עוד'
     }];
 
-    const [hidden, setHidden] = useState({display: "none"});
-    const [divBackground, setDivBackground] = useState({backgroundColor: "transparent"});
-    const [arrowDir, setArrowDir] = useState("nav-icons fa fa-arrow-right");
+    const [hidden, setHidden] = useState({display: 'none'});
+    const [divBackground, setDivBackground] = useState({backgroundColor: 'transparent'});
+    const [arrowDir, setArrowDir] = useState();
 
     const handlePhoneMenuClick = () => {
-        if (hidden.display === "inline") {
-            setHidden({display: "none"});
-            setDivBackground({backgroundColor: "transparent"});
-            setArrowDir("nav-icons fa fa-arrow-right");
-        } else {
-            setHidden({display:"inline"});
+        if (hidden.display === 'flex') {
+            setHidden({display: 'none'});
             setDivBackground({
-                backgroundColor: "rgba(198, 196, 199)",
-                opacity: "0.6",
-                width: "100%",
+                backgroundColor: 'transparent',
+                width: 'auto'
             });
-            setArrowDir("arrow-icon nav-icons fa fa-arrow-left");
+            setArrowDir({
+                transform: 'rotate(0deg)'
+            });
+        } else {
+            setHidden({display: 'flex'});
+            setDivBackground({
+                backgroundImage: 'linear-gradient(135deg, rgba(50, 51, 51, 1) 20%, rgba(223, 224, 224, 1) 100%)',
+                opacity: '0.8',
+                width: '100%',
+            });
+            setArrowDir({
+                transform: 'rotate(180deg)'
+            });
         }
     };
 
@@ -49,13 +56,15 @@ const NavBar = ({ srcs }) => {
                 <img src={headerGif} alt='neta abecsis' className='headerGif'/>
             </header>
             <nav className='phone-nav' style={divBackground}>
-                <button className={arrowDir} onClick={handlePhoneMenuClick} />
-                {phoneIcons.map((phoneIcon, i) => {
-                    const iconClass = `nav-icons fa fa-${phoneIcon.icon}`
-                    const iconLink = `#${phoneIcon.href}`
-                    const iconKey = `icon${i}`
-                    return <a href={iconLink} className={iconClass} style={hidden} key={iconKey}></a>
-                })}
+                <button className='arrow-icon nav-icons fa fa-arrow-right' onClick={handlePhoneMenuClick} style={arrowDir}/>
+                <div className='phone-menu-div' style={hidden}>
+                    {phoneIcons.map((phoneIcon, i) => {
+                        const iconClass = `nav-icons`
+                        const iconLink = `#${phoneIcon.href}`
+                        const iconKey = `icon${i}`
+                        return <a href={iconLink} className={iconClass} key={iconKey}><b>{' ' + phoneIcon.href}</b></a>
+                    })}
+                </div>
             </nav>
         </div>
     )
