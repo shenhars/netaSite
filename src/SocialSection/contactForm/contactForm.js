db35935f2fbe113d775f85342d93674d86af2a52
shenhars <shenhars03@gmail.com>
2023-06-19 19:15:35 +0300

    mail request fix

--- src/SocialSection/contactForm/contactForm.js
+++ src/SocialSection/contactForm/contactForm.js
@@ -1,50 +1,31 @@
 import './contactForm.css';
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
-    const [message, setMessage] = useState('');
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-        await axios.post(`https://netasite-server.onrender.com/sendMail`, {
-            name: name,
-            email: email,
-            subject: subject,
-            message: message
-        })
-        .then((res) => {
-            console.log(res);
-            e.target.reset();
-            alert('!נשלח');
-        })
-        .catch((err) => {
-            console.log(err);
-            alert('קרתה תקלה, נסה שוב מאוחר יותר');
-        });
         
-        // emailjs.sendForm('service_tv65c5d', 'template_lab00tg', e.target, 'Mg28HpnzbfTgc3sge')
-        //     .then((result) => {
-        //         console.log(result.text);
-        //         e.target.reset();
-        //         alert('נשלח!');
-        //     }, (error) => {
-        //         console.log(error.text);
-        //         alert('קרתה תקלה, נסה שוב מאוחר יותר');
-        // });
+        emailjs.sendForm('service_tv65c5d', 'template_lab00tg', e.target, 'Mg28HpnzbfTgc3sge')
+            .then((result) => {
+                console.log(result.text);
+                e.target.reset();
+                alert('נשלח!');
+            }, (error) => {
+                console.log(error.text);
+                alert('קרתה תקלה, נסה שוב מאוחר יותר');
+        });
     }
 
     return (
         <form className='contactSec' method='POST' onSubmit={handleSubmit}>
             <div className='innerContact'>
                 <h1 className='contactHeader'>:צור קשר</h1>
-                <input id='name' name='name' type='text' placeholder='שם מלא' className='contactInputs' onChange={e => setName(e.target.value)} required /><br/>
-                <input id='mail' name='mail' type='mail' placeholder='אימייל' className='contactInputs' onChange={e => setEmail(e.target.value)} required /><br />
-                <input id='subject' name='subject' type='text' placeholder='נושא' className='contactInputs' onChange={e => setSubject(e.target.value)} required /><br />
-                <textarea id='message' name='message' type='text' placeholder='הודעה' className='contactInputs' onChange={e => setMessage(e.target.value)} required /><br />
+                <input id='name' name='name' type='text' placeholder='שם מלא' className='contactInputs' required /><br/>
+                <input id='mail' name='mail' type='mail' placeholder='אימייל' className='contactInputs' required /><br />
+                <input id='subject' name='subject' type='text' placeholder='נושא' className='contactInputs' required /><br />
+                <textarea id='message' name='message' type='text' placeholder='הודעה' className='contactInputs' required /><br />
                 <button className='submissionContactButton form-button' type='submit'>שלח</button>
             </div>
         </form>
